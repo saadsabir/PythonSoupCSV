bd1b4c9887488558133bb1421606e4fd917a1bae
saadsabir <saadsabir9@gmail.com>
2023-01-03 12:01:06 +0100

    Update scrap_one_book
    update reviews_rating value
    update number_available value
    update full_image_url value
    run function create_csv

--- scrapOneBook.py
+++ scrapOneBook.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+import csv, os, urllib.request
+
 #function to get one book by parameters (string:url)
 #return list of details of the book
 def get_one_book(url):
@@ -11,6 +15,9 @@ def get_one_book(url):
         product_url =url
         image = soup.find('img')
         image_url = image.attrs['src']
+        full_image_url = ""
+        image_url = image_url[6:]  # remove first 6 char using python slicing
+        full_image_url = "http://books.toscrape.com/"+image_url
         titre = image.attrs['alt']
         product_description=soup.findAll('p')[3].text
         product_description=product_description.replace('\n','')
@@ -22,15 +29,25 @@ def get_one_book(url):
         price_excl_tax = table.findAll('td')[2].text
         price_incl_tax = table.findAll('td')[3].text
         tax =table.findAll('td')[4].text
-        number_available= table.findAll('td')[5].text
+        number_available = table.findAll('td')[5].text
+        number_available = number_available.split('(')
+        number_available = number_available[1]
+        number_available = number_available.split(' ')
+        number_available = number_available[0]
         reviews_rating = soup.find('p',class_='star-rating')
         reviews_rating = reviews_rating['class'][1]
+        if reviews_rating == 'One':
+           reviews_rating = 1
+        elif reviews_rating == 'Two':
+           reviews_rating = 2
+        elif reviews_rating == 'Three':
+           reviews_rating = 3
+        elif reviews_rating == 'Four':
+           reviews_rating = 4
+        elif reviews_rating == 'Five':
+           reviews_rating = 5
+               
         #get image of book
-        full_image_url =""
-        url1 = "http://books.toscrape.com/"
-        url2 = image_url
-        url2 = url2[6:] #remove first 6 char using python slicing
-        full_image_url = url1+url2
         image_name = product_url.split('/')
         image_name = image_name[4]
         image_name = image_name+".jpg"
@@ -43,26 +60,28 @@ def get_one_book(url):
         #download image
         urllib.request.urlretrieve(full_image_url,path+'/'+image_name)
 
-        return [product_url,upc,titre,price_incl_tax,price_excl_tax,number_available,product_description,category,reviews_rating,image_url]
+        return [product_url,upc,titre,price_incl_tax,price_excl_tax,number_available,product_description,category,reviews_rating,full_image_url]
      except Exception as e:
              return [e]
 
 
 #create a ccv file
 def create_csv(url):
- try:
-     en_tete = ["product_url","upc","titre","price_incl_tax","price_excl_tax","number_available","product_description","category","reviews_rating","image_url"]
-     fichier = "one_book.csv"
-     # checking if the directory csv exist or not.
-     if not os.path.exists("csv"):
-         # if the csv directory is not present then create it.
-         os.makedirs("csv")
-     with open(f'csv/{fichier}', 'w') as csv_file:
-      writer = csv.writer(csv_file, delimiter=',')
-      writer.writerow(en_tete)
-      writer.writerow(get_one_book(url))
- except Exception as e:
-     print(e)
+en_tete = ["product_url","upc","titre","price_incl_tax","price_excl_tax","number_available","product_description","category","reviews_rating","image_url"]
+fichier = "one_book.csv"
+# checking if the directory csv exist or not.
+if not os.path.exists("csv"):
+    # if the csv directory is not present then create it.
+    os.makedirs("csv")
+with open(f'csv/{fichier}', 'w') as csv_file:
+ writer = csv.writer(csv_file, delimiter=',')
+ writer.writerow(en_tete)
+ writer.writerow(get_one_book(url))
+
 
 #run function create_csv
-create_csv("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+try :
+  if __name__ == "__main__":
+    create_csv("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+except Exception as e:
+  print(e)
